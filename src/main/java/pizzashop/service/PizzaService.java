5a7ca05be59e2ab8ad5164fa983c9e2510863ddb
Arii_1 <arii.stan75@gmail.com>
2025-04-09 16:05:25 +0300

    update JUnit 5, Coverage 03

--- src/main/java/pizzashop/service/PizzaService.java
+++ src/main/java/pizzashop/service/PizzaService.java
@@ -40,11 +40,29 @@ public class PizzaService {
         payRepo.add(payment);
     }
 
+//    public double getTotalAmount(PaymentType type){
+//        double total=0.0f;
+//        List<Payment> l=getPayments();
+//
+//        if ((l==null) ||(l.isEmpty()))
+//            return total;
+//
+//        for (Payment p:l){
+//            if (p.getType().equals(type))
+//                total+=p.getAmount();
+//        }
+//
+//        return total;
+//    }
+
     public double getTotalAmount(PaymentType type){
         double total=0.0f;
         List<Payment> l=getPayments();
 
-        if ((l==null) ||(l.isEmpty()))
+        if (l==null)
+            return total;
+
+        if (l.isEmpty())
             return total;
 
         for (Payment p:l){
